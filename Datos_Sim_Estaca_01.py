b42ae8081d9868c785818ff870abee4e01021f02
SuSuGy11 <132923752+SahiG0@users.noreply.github.com>
2025-09-24 21:33:48 -0300

    Cambios

--- Datos_Sim_Estaca_01.py
+++ Datos_Sim_Estaca_01.py
@@ -1,22 +1,20 @@
 import datetime
 
-# DEFINÍ la plantilla ANTES del for
 simulated_template = {
     "device_id": "device_01",
-    "sensor": "temperature",
-    "value": 20.0,
-    "unit": "C"
+    "sensor": "temperatura",
+    "variable": 20.0,
+    "unidad": "C"
 }
 
 data_list = []
 for i in range(1000):
-    # .copy() funciona porque simulated_template es un dict
     item = simulated_template.copy()
-    item["value"] = round(simulated_template["value"] + i * 0.01, 2)
+    item["variable"] = round(simulated_template["variable"] + i * 0.01, 2)
     item["seq"] = i + 1
-    item["timestamp"] = datetime.datetime.utcnow().isoformat() + "Z"
+    item["tiempo estimado"] = datetime.datetime.utcnow().isoformat() + "Z"
     data_list.append(item)
 
-print("Generados:", len(data_list))
-print("Ejemplo:", data_list[0])
+print("Datos Generados:", len(data_list))
+print("Ejemplo:", data_list[1])
 
